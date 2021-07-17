528c2d5f2fffad5645bdb81b17f75c43777af387
andrey <andrey_belyanin@mail.ru>
2021-07-17 13:03:48 +0200

    DB URL changed

--- ad_api_on_flask/app.py
+++ ad_api_on_flask/app.py
@@ -12,10 +12,11 @@ from schema import AD_CREATE, USER_CREATE
 
 app = Flask(__name__)
 jwt = JWTManager(app)
-Heroku_DB_URL = (subprocess.getstatusoutput('heroku config --app my-flask-api-ci'))[1].split(' ')[-1]
+# Heroku_DB_URL = (subprocess.getstatusoutput('heroku config --app my-flask-api-ci'))[1].split(' ')[-1]
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{}:{}@localhost:5432/ad_api'.format(os.getenv('DB_USER'),
 #                                                                                           os.getenv('DB_PASSWORD'))
-app.config['SQLALCHEMY_DATABASE_URI'] = Heroku_DB_URL.replace('postgres', 'postgresql+psycopg2')
+# app.config['SQLALCHEMY_DATABASE_URI'] = Heroku_DB_URL.replace('postgres', 'postgresql+psycopg2')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://zztahwzrzkehqk:38c9ad1b6f88d9ae5ec5a69a434f204afaf38de03b306f042236cd25b846d772@ec2-54-74-156-137.eu-west-1.compute.amazonaws.com:5432/de9hvc2jkmlvnh'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 SALT = 'my_salt'
 db = SQLAlchemy(app)
